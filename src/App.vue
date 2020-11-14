f550fcce8efe004b98b0370aa6df798c907a0561
Jared Martinez <jared97martinez@gmail.com>
2020-11-14 13:38:50 -0800

    Project set up
    Set up for the project.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <app-header />
+    </v-app-bar>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: { AppHeader },
+  data() {},
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container fluid>
+        <v-row justify="center">
+            <h2>IP Address Tracker</h2>
+        </v-row>
+    </v-container>
+</template>
